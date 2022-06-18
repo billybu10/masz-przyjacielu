655b3f4d66aa83eb753c3312cb65de521c1a1446
billybu10 <101750525+billybu10@users.noreply.github.com>
2022-06-18 10:04:59 +0200

    Add files via upload

---
+++ PDZ/metody/lotto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, liczba;
+            for (i = 0; i<=48; i++)
+            {
+                Console.WriteLine(i + 1 + " ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Ilosc elementow : ");
+            n = int.Parse(Console.ReadLine());
+            if (n >= 49)
+            {
+                Console.WriteLine("Nie");
+            }
+            else
+            {
+                Console.WriteLine(" ");
+                List<int> wylosowane = new List<int>();
+                for (i = 0; i < n; i++)
+                {
+                    liczba = wylosuj_liczbe();
+
+                    while (wylosowane.Contains(liczba))
+                    {
+                        liczba = wylosuj_liczbe();
+                    }
+                    wylosowane.Add(liczba);
+                    Console.WriteLine(" liczba : " + liczba);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static int wylosuj_liczbe()
+        {
+            int liczba;
+            Random x = new Random((int)DateTime.Now.Ticks);
+            int y = x.Next(1, 49);
+            liczba = 1 + y % 49;
+            return liczba;
+        }
+    }
+}
---
+++ PDZ/metody/metody/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=0;
+            Console.WriteLine("Podaj zakres od 0 do:");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                if (czyparzysta(i) == true)
+                {
+                    Console.WriteLine(i + " Ta liczba jest parzysta.");
+                }
+                else
+                {
+                    Console.WriteLine(i + " Ta liczba nie jest parzysta.");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static bool czyparzysta(int liczba)
+        {
+            bool parzysta;
+            if ((liczba % 2) == 0)
+            {
+                parzysta = true;
+            }
+            else
+            {
+                parzysta = false;
+            }
+            return parzysta;
+        }
+    }
+}
