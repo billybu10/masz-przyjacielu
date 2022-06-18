42879f7406f1fcbbbcf211794843327c526c5bfa
billybu10 <101750525+billybu10@users.noreply.github.com>
2022-06-18 10:08:13 +0200

    Add files via upload

---
+++ PDZ/Zadania/BMI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bmi = 0;
+            double waga = 0;
+            double cm = 0;
+            Console.WriteLine("Podaj wzrost w cm: ");
+            cm = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wagę w kg: ");
+            waga = double.Parse(Console.ReadLine());
+
+            double wzrost2= ((cm / 100)*(cm/100));
+            Console.WriteLine(wzrost2);
+
+            bmi = (waga / wzrost2);
+
+            Console.WriteLine("Twoje BMI wynosi: " + bmi); /*{0:F2} pozwala na wypisanie zmiennej typu double*/
+
+            if (bmi < 18)
+            {
+                Console.WriteLine("Masz niedowagę. :(");
+            }
+            else if((bmi >=18)&&(bmi <= 25))
+            {
+                Console.WriteLine("Jest OK :)");
+            }
+            else if (bmi > 25)
+            {
+                Console.WriteLine("Masz nadwagę. :(");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PDZ/Zadania/Zadania/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s= 0, n, ile = 0;
+            Console.WriteLine("Podaj ilość elementów");
+            n = int.Parse(Console.ReadLine());
+            Random x = new Random();
+            int i = 1;
+
+
+            while (i <= n)
+            {
+                int liczba = x.Next(1, 100);
+                if ((liczba >= 30)&&(liczba <= 60))
+                {
+                    Console.WriteLine("Liczba" + liczba);
+                    s = s + liczba;
+                    ile++;
+                    i++;
+                }
+            }
+            Console.WriteLine("Suma elementów wynosi: " + s + ". Ilość elementów: " + ile);
+            Console.WriteLine($"Suma elementów wynosi: {s} . Ilość elementów: {ile}");
+            Console.WriteLine("Suma elementów wynosi: {0}. Ilość elementów: {1}", s, ile);
+            Console.ReadLine();
+        }
+    }
+}
